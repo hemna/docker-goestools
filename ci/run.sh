2ed5d6fa22f2796ae1a1c5e4a53934c1ab0a39ef
Hemna <waboring@hemna.com>
2020-05-04 15:52:52 -0400

    Update to hide the secrets

--- ci/run.sh
+++ ci/run.sh
@@ -7,8 +7,13 @@ export fly_target=${fly_target:-hemna}
 echo "Concourse API target ${fly_target}"
 echo "Hemna $(basename $DIR)"
 
+if [ ! -f credentials.yml ]; then
+    echo "You need to create credentials.yml"
+    exit 1
+fi
+
 pushd $DIR
-  fly  -t ${fly_target} set-pipeline -p goes -c pipeline.yml -n
-  fly  -t ${fly_target} unpause-pipeline -p goes
-  fly --verbose -t ${fly_target} trigger-job -w -j goes/build-and-publish-goestools
+  fly -t ${fly_target} set-pipeline -p goes -c pipeline.yml -l credentials.yml
+  fly -t ${fly_target} unpause-pipeline -p goes
+  fly -t ${fly_target} trigger-job -w -j goes/build-and-publish-goestools
 popd
