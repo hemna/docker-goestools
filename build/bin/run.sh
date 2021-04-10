842b201732eb06deae6a00a688203c4f4482095e
Hemna <waboring@hemna.com>
2021-04-09 20:49:21 -0400

    nuked

--- build/bin/run.sh
+++ build/bin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-telegraf --config /etc/telegraf/telegraf.conf --quiet &
+#telegraf --config /etc/telegraf/telegraf.conf --quiet &
 
 cd $HOME/data
 goesproc -c /home/goes/goesproc.conf --mode packet --subscribe tcp://192.168.1.7:5004
